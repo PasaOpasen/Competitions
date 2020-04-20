212aab084b858ddf599ad61c8215e7202e485e78
ДМ. Па <qtckpuhdsa@gmail.com>
2020-04-20 23:33:05 +0300

    try simplest models

--- Ion Switching/visual.R
+++ Ion Switching/visual.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(data.table)
 library(magrittr)
+library(caret)
+library(MLmetrics)
+library(doSNOW)
+
 
 data.path='D:/liverpool-ion-switching/'
 
@@ -8,39 +12,111 @@ train.path=paste0(data.path,'train','.csv')
 test.path=paste0(data.path,'test','.csv')
 sample.path=paste0(data.path,'sample_submission','.csv')
 
+
 dt=read_csv(train.path)
 
 dt %>% summary()
 
-dt$open_channels %>% factor() %>% summary()
+dt %<>% mutate(chan=factor(open_channels,ordered = T),chan2=factor(paste0('l',open_channels)))
+
+dt$chan %>% summary()
+
+
+data=dt %>% group_by(chan) %>% slice(sample(1:n(),30000))
+ggplot(data,aes(x=time,y=signal,col=chan))+geom_point()
+
+
+dt %>% slice(seq(1,n(),by=200)) %>% ggplot(aes(x=time,y=signal,col=chan))+geom_point()
+
+
+simple.logres=lm(open_channels~signal+signal:time,dt)
+simple.logres %>% summary()
 
 
 
-dt %<>% mutate(chan=factor(open_channels,ordered = T))
+
+calcF1Scores=function(act,prd){
+    #treats the vectors like classes
+    #act and prd must be whole numbers
+    df=data.frame(act=as.numeric(act),prd=as.numeric(prd));
+    scores=numeric(11);
+    for(i in 0:10){
+      
+      tp=nrow(df[df$prd==i & df$act==i,]);        
+      fp=nrow(df[df$prd==i & df$act!=i,]);
+      fn=nrow(df[df$prd!=i & df$act==i,]);
+
+      scores[i+1]=(2*tp)/(2*tp+fp+fn)
+    }      
+    return(mean(scores))
+}
+calcF1Scores2=function(act,prd){
+  #treats the vectors like classes
+  #act and prd must be whole numbers
+  df=data.frame(act=act,prd=prd);
+  scores=numeric(11);
+  lv=levels(prd)
+  for(i in 0:10){
+    ii=lv[i+1]
+    tp=nrow(df[df$prd==ii & df$act==ii,]);        
+    fp=nrow(df[df$prd==ii & df$act!=ii,]);
+    fn=nrow(df[df$prd!=ii & df$act==ii,]);
+    
+    scores[i+1]=(2*tp)/(2*tp+fp+fn)
+  }      
+  #print(scores)
+  return(mean(scores,na.rm = T))
+}
+
+f1 <- function (data, lev = NULL, model = NULL) {
+
+  answ=calcF1Scores2(data$obs,data$pred)
+  names(answ)="F1"
+ return(answ)
+
+} 
+
+tr=trainControl(
+  method = 'cv',
+  number=10,
+  summaryFunction = f1,
+  classProbs = T,
+  verboseIter = T
+  )
 
 
-dt %>% slice(seq(1,n(),by=500)) %>% ggplot(aes(x=time,y=signal,col=chan))+geom_point()
+d=seq(1,nrow(dt),by=300)
+
+fit.svm=train(x=data[c(1,2)],y=data$chan2,
+              method='lda2',
+              trControl = tr,
+              maximize = T,
+              metric = 'F1')
 
 
-simple.logres=glm(chan~signal,dt,family =gaussian )
+calcF1Scores2(dt$chan2,fit.svm %>% predict(dt))
+
+
 
 
 
 test=read_csv(test.path)
 
+res=fit.svm %>% predict(test) %>% str_sub(2) %>% as.numeric()
 
 
 
+
+#writing_sample
 
 answer=read_csv(sample.path)
-answer$open_channels=sample(1:10,answer$time %>% length(),replace = T)
-write_csv(answer,"result.csv")
+
+answer$time=format(answer$time,nsmall = 4)
+#answer$open_channels=sample(0:10,answer$time %>% length(),replace = T)
+answer$open_channels=res
 
 
-
-
-
-
+write_csv(answer,paste0(data.path,"result.csv"))
 
 
 
