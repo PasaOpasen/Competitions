9e44a419b3d0e8cc6a6b926defcf4cf789cba790
ДМ. Па <qtckpuhdsa@gmail.com>
2020-04-21 20:33:30 +0300

    new predictions

--- Ion Switching/first_example.R
+++ Ion Switching/first_example.R
@@ -1,5 +1,6 @@
 
 #example from https://www.kaggle.com/jpbeconne/beginner-notebook
+#https://www.kaggle.com/kittlein/clean-signal-and-naive-features
 
 library(tidyverse)
 library(data.table)
@@ -7,6 +8,19 @@ library(magrittr)
 library(caret)
 library(MLmetrics)
 library(doSNOW)
+library(data.table)
+library(readr)
+library(tidyverse)
+library(xgboost)
+library(RcppRoll)
+library(dse)
+library(KFAS)
+library(wmtsa)
+library(onehot)
+#library(devtools)
+#install_github("andrewuhl/RollingWindow")
+#library(RollingWindow)
+library(tsfeatures)
 
 
 f1 <- function (data, lev = NULL,model=NULL) {
@@ -41,13 +55,88 @@ train_data$time_batch <- ((train_data$time - 0.0001)%%50)+0.0001
 
 
 
+#experiment predictions#####
+number_peaks <- function(x, window){
+  rollmax <- as.numeric(roll_max(x, window))
+  npeak <- sum(x > lead(rollmax, n = window) & x > lag(rollmax), na.rm = T)
+  return(npeak)
+}
+
+# fast removal of na values
+Rep.Nas = function(DT) {
+  for (i in names(DT))
+    DT[is.na(get(i)), (i):=0]
+}
+
+decode=function(x){
+  bre=quantile(x, seq(0.1, 0.9, by=0.05))
+  clase=rep(NA, length(x))
+  for(j in 1:length(bre)){
+    indi= which(x >=  bre[j] & x < bre[j+1]) 
+    clase[indi]=j
+  }
+  return(clase)
+}
+
+# Get count of signal values around mean of open channels values
+msoc=as.vector(by(train_data$signal, train_data$open_channels, mean))
+
+freChann=function(x, chann){
+  indi= which(x >  msoc[chann] - 0.1 & x < msoc[chann] + 0.1)
+  return(length(indi))
+}
+
+# Smooth signal
+mkSmoo=function(x, bw){
+  sk=ksmooth(seq(0, 10, length=length(x)), x, bandwidth = bw)
+  return(sk$y)
+}
+
+train_data %<>% group_by(batches) %>% mutate(
+  mKsmoo2 = mkSmoo(signal, bw=0.0025),
+  cls1 = decode(signal), 
+  signalSquare = signal^2,
+  cls2=decode(signalSquare), 
+  signale33 = sign(signal)*abs(signal)^(1/3),
+  cls3= decode(signale33)
+)
+
+train_data %<>%
+  mutate(
+ # RollingMaxB10 = roll_max(signal, n = 10, fill=max(signal)),
+  RollingMaxB20= roll_max(signal, n = 20, fill=max(signal))
+) %>% 
+  group_by(batches) %>% mutate(
+   RollingMeanB10 = roll_mean(signal, n = 10, fill=mean(signal)),
+   q4B = length(which(signal > 0 & signal < 1.5)==T), 
+   #mB =mean(signal), 
+   #maxB = max(signal), 
+   RollingMaxB10 = roll_max(signal, n = 10, fill=max(signal)), 
+  #abs_avgBs2 = (abs(min(signal)) + abs(max(signal)))/2, 
+   
+   #abs_avgB = (abs(min(signal)) + abs(max(signal)))/2,
+    
+   #q5B = length(which(signal > 0 & signal < 1.5)==T),
+   shiftM1 = shift(signal, n=1, fill=mean(signal), "lag"), 
+   shiftM2 = shift(signal, n=2, fill=mean(signal), "lag"), 
+   shiftP1 = shift(signal, n=1, fill=mean(signal), "lead"), 
+  shiftP2 = shift(signal, n=2, fill=mean(signal), "lead")
+)
+
+for(name in colnames(train_data)){
+  t = train_data[,name] %>% unlist()
+  train_data[is.na(t) | is.nan(t),name]=0
+}
+
+############
+
 
 ggplot(train_data %>% slice(seq(1,n(),400)),
        aes(x=time_batch,y=signal,col=open_channels))+geom_point()+facet_grid(vars(batches))
 
 
 
-
+#estimate trends####
 
 train_data$trend_res<-0
 train_data$trend_spl<-0
@@ -60,8 +149,8 @@ for(lBatch in c(0,2:5,6:9)){
     trend.lm<-lm(signal~time_batch,data=subset(train_data,batches==lBatch))
   }
   
-  cat("batch is",lBatch,'\n')
-  summary(trend.lm) %>% print()
+  #cat("batch is",lBatch,'\n')
+  #summary(trend.lm) %>% print()
   
   train_data$trend_res[train_data$batches==lBatch] <- trend.lm$residuals
   train_data$trend_spl[train_data$batches==lBatch]<-trend.lm$fitted.values
@@ -74,7 +163,7 @@ train_data$trend_res[inds]<-trend.lm$residuals
 train_data$trend_spl[inds]<-trend.lm$fitted.values
 train_data$piece[inds]<-"11"
 
-summary(trend.lm) %>% print()
+#summary(trend.lm) %>% print()
 
 inds=train_data$batches==1 & train_data$time_batch>10
 trend.lm<-lm(signal~time_batch,data=subset(train_data,inds))
@@ -82,7 +171,7 @@ train_data$trend_res[inds]<-trend.lm$residuals
 train_data$trend_spl[inds]<-trend.lm$fitted.values
 train_data$piece[inds]<-"12"
 
-summary(trend.lm) %>% print()
+#summary(trend.lm) %>% print()
 
 
 
@@ -92,6 +181,7 @@ ggplot(train_data %>% slice(seq(1,n(),400)),
 
 
 
+#modeling####
 
 cv5<- trainControl(method="cv",number=5,summaryFunction = f1)
 train_ix<-createDataPartition(train_data$signal,p = 0.8,list = F)[,1]
@@ -104,12 +194,31 @@ lda.model02
 lda.valid02<-predict(lda.model02,newdata = test)
 f1(data.frame(obs=test$open_channels,pred=lda.valid02))
 
+
 lda.model03 <- train(open_channels~trend_res+batches,data=train,method="lda",family="binomial", trControl=cv5,verbosity=T,metric="F1")
 lda.model03
+
+lda.model03=train(open_channels~trend_res+trend_spl+time_batch+batch,
+                  data=train,
+                  method="lda",
+                  family="binomial", 
+                  trControl=cv5,
+                  verbosity=T,
+                  metric="F1")
+
 lda.valid03<-predict(lda.model03,newdata = test)
+
+table(test$open_channels, lda.valid03)
+
 f1(data.frame(obs=test$open_channels,pred=lda.valid03))
 
 
+
+
+
+
+
+#test predictions####
 
 # for predict test values u should compare
 # test signal amplitude for each batch with test ones
@@ -120,7 +229,9 @@ f1(data.frame(obs=test$open_channels,pred=lda.valid03))
 #
 #
 
+######## default batches
 test_data$batches<- factor((test_data$time - 0.0001)%/%50-10,levels=levels(train_data$batches))
+
 test_data$time_batch <- ((test_data$time - 0.0001)%%50)+0.0001
 
 ggplot(test_data %>% slice(seq(1,n(),300)),
@@ -154,6 +265,10 @@ ggplot(test_data %>% slice(seq(1,n(),300)),
        aes(x=time_batch,y=trend_res))+geom_point()+facet_grid(vars(batches))
 
 
+
+
+
+
 #find best combinations by plots
 tr=train_data %>% slice(seq(1,n(),600))
 te=test_data %>% slice(seq(1,n(),400))
@@ -172,14 +287,16 @@ for(i in 0:3){
 
 
 # use optimal batches
-test_data$batches[test_data$batches==0]=6
+test_data$batches[test_data$batches==0]=7
 test_data$batches[test_data$batches==1]=4
 test_data$batches[test_data$batches==2]=6
-test_data$batches[test_data$batches==3]=0
+test_data$batches[test_data$batches==3]=2
 
 
 
 #best model predictions
+
+#как сделать retrain модели?
 
 lda.model03 <- train(open_channels~trend_res+batches,data=train_data,method="lda",family="binomial", trControl=cv5,verbosity=T,metric="F1")
 
@@ -195,7 +312,7 @@ answer$time=format(answer$time,nsmall = 4)
 answer$open_channels=res
 
 
-write_csv(answer,paste0(data.path,"result.csv"))
+write_csv(answer,paste0(data.path,"result7462.csv"))
 
 
 
